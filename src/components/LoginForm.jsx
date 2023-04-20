754be3549d9b0eb7e6986082af1a0d2f9862f93a
pytest <pytest514@gmail.com>
2023-04-20 22:53:16 +0800

    feat: update user login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = { email, password };
+    console.log(user);
+  };
   return (
     <>
-      <h1>Login Form</h1>
-      <form>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
         <input
           name="email"
@@ -14,9 +20,9 @@ export default function LoginForm() {
         ></input>
         <label htmlFor="email">Password</label>
         <input
-          name="email"
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
+          name="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
         ></input>
         <button>Submit</button>
       </form>

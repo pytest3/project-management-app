7489f4f539721b91325006412cd7128e446c6031
pytest <pytest514@gmail.com>
2023-04-20 23:21:42 +0800

    feat: set up firebase config for auth

---
+++ src/firebase/firebase-config.jsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDvQbV38TaooOuLIgEqGlfVrx62oJb03U",
+  authDomain: "project-management-fe061.firebaseapp.com",
+  projectId: "project-management-fe061",
+  storageBucket: "project-management-fe061.appspot.com",
+  messagingSenderId: "848586280601",
+  appId: "1:848586280601:web:62ff63bd46db6be71bb208",
+};
+
+const app = initializeApp(firebaseConfig);

d2a38cae03cc35811ea36ad725cf5695fc5db388
pytest <pytest514@gmail.com>
2023-04-22 22:56:48 +0800

    feat: add firebase auth by using useSignUp hook

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
+import useSignUp from "../hooks/use-signup";
+
 export default function SignUpForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { error, pending, signUp } = useSignUp();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    signUp(email, password);
+  };
   return (
     <>
       <h1>Signup Form</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
-        <input name="email" type="email"></input>
-        <label htmlFor="email">Password</label>
-        <input name="email" type="email"></input>
-        <button>Sign Up</button>
+        <input
+          name="email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <button>Submit</button>
       </form>
     </>
   );

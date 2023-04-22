e481d696c903f2544b3038879d5344358689c885
pytest <pytest514@gmail.com>
2023-04-22 23:19:59 +0800

    feat: add logged in status and log out button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,25 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import useLogOut from "../hooks/use-logout";
+import { AuthContext } from "../store/auth-context";
+import { useContext } from "react";
 
 export default function NavBar() {
+  const { logOut } = useLogOut();
+  const authCtx = useContext(AuthContext);
+  const { user } = authCtx;
+
   return (
     <Wrapper>
       <h1>PM</h1>
+
+      {user && <div>{user.email} is logged in </div>}
       <NavbarActions>
         <Link to="/">Login</Link>
-        <Link to="/signup">Create user</Link>
+        <Link to="/" onClick={logOut}>
+          Logout
+        </Link>
+        <Link to="/signup">Signup</Link>
       </NavbarActions>
     </Wrapper>
   );
@@ -15,7 +27,7 @@ export default function NavBar() {
 
 const Wrapper = styled.nav`
   display: grid;
-  grid-template-columns: repeat(2, auto);
+  grid-template-columns: repeat(3, auto);
   justify-content: space-between;
 `;
 

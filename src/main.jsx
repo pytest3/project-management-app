134180c3e338ff56fdf15151e3ec7f1f79b758f9
pytest <pytest514@gmail.com>
2023-04-22 22:53:46 +0800

    feat: include context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { AuthContextProvider } from "./store/auth-context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App></App>
+    <AuthContextProvider>
+      <App></App>
+    </AuthContextProvider>
   </React.StrictMode>
 );

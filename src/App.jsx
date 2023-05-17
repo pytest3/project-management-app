7c457e61a608b6e5f1dea68fa987c5852d7bf4c9
pytest <pytest514@gmail.com>
2023-05-17 18:11:28 +0800

    feat: add auth state listener to monitor for active user

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import Root from "./pages/root";
 import SignUpForm from "./components/SignUpForm";
+import { useContext, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import auth from "./firebase/firebase-config";
+import { AuthContext } from "./store/auth-context";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +19,20 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const authCtx = useContext(AuthContext);
+  const { dispatch } = authCtx;
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch({ type: "login", payload: user });
+      } else {
+        dispatch({ type: "logout", payload: null });
+      }
+    });
+    return () => unSub();
+  }, [dispatch]);
+
   return <RouterProvider router={router} />;
 }
 

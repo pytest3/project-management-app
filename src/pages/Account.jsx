b8292fb863aa317ecbc9684d4220a5589620da2f
pytest <pytest514@gmail.com>
2023-05-22 21:30:34 +0800

    feat: add event listener for form input

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,36 +1,63 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function AccountPage() {
+  const [profile, setProfile] = useState({
+    email: "",
+    password: "",
+    gender: "",
+    country: "",
+  });
+
+  function handleEmailChange(e) {
+    setProfile({ ...profile, email: e.target.value });
+  }
+  function handlePasswordChange(e) {
+    setProfile({ ...profile, password: e.target.value });
+  }
+  function handleGenderChange(e) {
+    setProfile({ ...profile, gender: e.target.value });
+  }
+  function handleCountryChange(e) {
+    setProfile({ ...profile, country: e.target.value });
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <>
-      <form>
-        <Field>
-          <Label htmlFor="email">Email address</Label>
-          <input type="text" id="email"></input>
-        </Field>
-        <Field>
-          <Label htmlFor="password">Update password</Label>
-          <input type="numbers" id="password"></input>
-          <Info>Password must be at least 8 characters long</Info>
-        </Field>
-        <Field>
-          <Label htmlFor="gender">Gender</Label>
-          <select type="" id="gender">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
-          <Info>
-            This information may be used to improve your recommendations and ads
-          </Info>
-        </Field>
-        <Field>
-          <Label htmlFor="country">Country</Label>
-          <input type="text" id="country"></input>
-          <Info>This is your primary location</Info>
-        </Field>
-        <SubmitButton>Submit</SubmitButton>
-      </form>
-    </>
+    <form onSubmit={handleFormSubmit}>
+      <Field>
+        <Label htmlFor="email">Email address</Label>
+        <input type="text" id="email" onChange={handleEmailChange}></input>
+      </Field>
+      <Field>
+        <Label htmlFor="password">Update password</Label>
+        <input
+          type="numbers"
+          id="password"
+          onChange={handlePasswordChange}
+        ></input>
+        <Info>Password must be at least 8 characters long</Info>
+      </Field>
+      <Field>
+        <Label htmlFor="gender">Gender</Label>
+        <select type="" id="gender" onChange={handleGenderChange}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+        <Info>
+          This information may be used to improve your recommendations and ads
+        </Info>
+      </Field>
+      <Field>
+        <Label htmlFor="country">Country</Label>
+        <input type="text" id="country" onChange={handleCountryChange}></input>
+        <Info>This is your primary location</Info>
+      </Field>
+      <SubmitButton>Submit</SubmitButton>
+    </form>
   );
 }
 

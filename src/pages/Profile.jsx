bab2fa23c45c1e8a9d003091a690c217d7501132
pytest <pytest514@gmail.com>
2023-05-22 22:50:45 +0800

    style: restyle profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,36 @@
+import styled from "styled-components";
+
 export default function ProfilePage() {
   return (
-    <>
-      <ul>
-        <li>
-          <h1>Add/Change Profile Picture</h1>
-          <button>Choose image</button>
-          <button>Upload image</button>
-        </li>
-        <li>
-          <h1>Bio</h1>
-          <label htmlFor="bio">Describe yourself</label>
-          <textarea id="bio" name="bio" rows="4" cols="50"></textarea>
-        </li>
-      </ul>
-    </>
+    <Wrapper>
+      <ProfileField>
+        <Header>Add/Change Profile Picture</Header>
+        <button>Choose image</button>
+        <button>Upload image</button>
+      </ProfileField>
+      <ProfileField>
+        <Header>Bio</Header>
+        <Label htmlFor="bio">Describe yourself</Label>
+        <textarea id="bio" name="bio" rows="4" cols="50"></textarea>
+      </ProfileField>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  padding-top: 16px;
+`;
+
+const Header = styled.h1`
+  font-size: 20px;
+  line-height: 1;
+  padding-bottom: 8px;
+`;
+
+const ProfileField = styled.article`
+  &:not(:last-of-type) {
+    padding-bottom: 16px;
+  }
+`;
+
+const Label = styled.label``;

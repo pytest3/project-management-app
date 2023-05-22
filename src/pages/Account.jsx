5b5d108e244ff5a1eb3a9109e3d9ce10b801b8fa
pytest <pytest514@gmail.com>
2023-05-22 17:02:11 +0800

    feat: add new fields in account page and style

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,30 +1,60 @@
+import styled from "styled-components";
+
 export default function AccountPage() {
   return (
     <>
-      <ul>
-        <article>
-          <h1>Email address</h1>
-          <p></p>
-          <button>Change</button>
-        </article>
-        <article>
-          <h1>Change password</h1>
-          <p>Password must be at least 8 characters long</p>
-          <button>Change</button>
-        </article>
-        <article>
-          <h1>Gender</h1>
-          <p>
+      <form>
+        <Field>
+          <Label htmlFor="email">Email address</Label>
+          <input type="text" id="email"></input>
+        </Field>
+        <Field>
+          <Label htmlFor="password">Update password</Label>
+          <input type="numbers" id="password"></input>
+          <Info>Password must be at least 8 characters long</Info>
+        </Field>
+        <Field>
+          <Label htmlFor="gender">Gender</Label>
+          <select type="" id="gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+          <Info>
             This information may be used to improve your recommendations and ads
-          </p>
-          <button>Change</button>
-        </article>
-        <article>
-          <h1>Country</h1>
-          <p>This is your primary location</p>
-          <button>Change</button>
-        </article>
-      </ul>
+          </Info>
+        </Field>
+        <Field>
+          <Label htmlFor="country">Country</Label>
+          <input type="text" id="country"></input>
+          <Info>This is your primary location</Info>
+        </Field>
+        <SubmitButton>Submit</SubmitButton>
+      </form>
     </>
   );
 }
+
+const Label = styled.div`
+  font-weight: 550;
+`;
+const Info = styled.p`
+  font-size: 12px;
+  color: gray;
+`;
+const Field = styled.div`
+  display: grid;
+  padding: 8px 0;
+
+  &:first-of-type {
+    padding-top: 28px;
+  }
+`;
+
+const SubmitButton = styled.button`
+  margin: 8px 0px;
+  padding: 3px 8px;
+  line-height: 1;
+  border-radius: 8px;
+  border: 1px solid gray;
+  font-weight: 550;
+`;

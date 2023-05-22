c7a9cb5b30c897dde3b936d74967f541650fb15d
pytest <pytest514@gmail.com>
2023-05-22 11:16:49 +0800

    feat: dispatch a login action once  user signs in

--- src/hooks/use-login.jsx
+++ src/hooks/use-login.jsx
@@ -1,26 +1,27 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import auth from "../firebase/firebase-config";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AuthContext } from "../store/auth-context";
 
 export default function useLogin() {
-  const [user, setUser] = useState();
   const [error, setError] = useState();
   const [pending, setPending] = useState();
+
+  const authCtx = useContext(AuthContext);
+  const { dispatch } = authCtx;
 
   const login = (email, password) => {
     setPending(true);
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-        setUser(user);
+        dispatch({ type: "login", payload: userCredential.user });
       })
       .catch((error) => {
         setError(error.message);
-        console.log(error);
       });
     setPending(false);
+    setError(null);
   };
 
-  return { login, user, error, pending };
+  return { login, error, pending };
 }

ad1f91f2d9c52bff337251410679b1ace90a1c2d
pytest <pytest514@gmail.com>
2023-05-22 11:11:14 +0800

    fix:  rest inputs and add error page
    Reset inputs to blank after form submit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,19 +2,26 @@ import { useState } from "react";
 import useLogin from "../hooks/use-login";
 import { useContext } from "react";
 import { AuthContext } from "../store/auth-context";
+import { Navigate } from "react-router-dom";
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { login } = useLogin();
+  const { login, error } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     login(email, password);
+    setEmail("");
+    setPassword("");
   };
+
+  const { user } = useContext(AuthContext);
 
   return (
     <>
+      {error && <p>{error}</p>}
+      {user && <Navigate replace to="/home"></Navigate>}
       <h1>Login Page</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
@@ -22,12 +29,14 @@ export default function LoginForm() {
           name="email"
           type="email"
           onChange={(e) => setEmail(e.target.value)}
+          value={email}
         ></input>
         <label htmlFor="password">Password</label>
         <input
           name="password"
           type="password"
           onChange={(e) => setPassword(e.target.value)}
+          value={password}
         ></input>
         <button>Submit</button>
       </form>

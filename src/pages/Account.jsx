4a7aa308712464d3f0024e774d0523be9c6086e8
pytest <pytest514@gmail.com>
2023-05-22 13:21:39 +0800

    feat: include more user info in account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,9 +2,28 @@ export default function AccountPage() {
   return (
     <>
       <ul>
-        <li>Personal details</li>
-        <li>Password and security</li>
-        <li>Ad prefence</li>
+        <article>
+          <h1>Email address</h1>
+          <p></p>
+          <button>Change</button>
+        </article>
+        <article>
+          <h1>Change password</h1>
+          <p>Password must be at least 8 characters long</p>
+          <button>Change</button>
+        </article>
+        <article>
+          <h1>Gender</h1>
+          <p>
+            This information may be used to improve your recommendations and ads
+          </p>
+          <button>Change</button>
+        </article>
+        <article>
+          <h1>Country</h1>
+          <p>This is your primary location</p>
+          <button>Change</button>
+        </article>
       </ul>
     </>
   );

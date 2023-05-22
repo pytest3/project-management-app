3346ac9f676a6aaea6e1d85f395a139b30b54e35
pytest <pytest514@gmail.com>
2023-05-22 11:17:50 +0800

    style: make content height 100%  screen height

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, Navigate } from "react-router-dom";
 import NavBar from "../components/Navbar";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../store/auth-context";
+import Home from "./Home";
+import styled from "styled-components";
 
 export default function Root() {
   return (
-    <div>
+    <Wrapper>
       <NavBar></NavBar>
       <Outlet></Outlet>
-    </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  height: 100%;
+`;

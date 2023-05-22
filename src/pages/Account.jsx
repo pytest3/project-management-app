9b36664839fcd473a1784d0da29c56ac9b7e1d44
pytest <pytest514@gmail.com>
2023-05-22 11:14:27 +0800

    initial commit

---
+++ src/pages/Account.jsx
@@ -0,0 +1,11 @@
+export default function AccountPage() {
+  return (
+    <>
+      <ul>
+        <li>Personal details</li>
+        <li>Password and security</li>
+        <li>Ad prefence</li>
+      </ul>
+    </>
+  );
+}

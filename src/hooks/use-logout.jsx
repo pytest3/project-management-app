96f79f7cc4bfc2905434cafd8bd37b55282cfb0b
pytest <pytest514@gmail.com>
2023-05-22 11:17:11 +0800

    feat: dispatchs logout action after user signs ou

--- src/hooks/use-logout.jsx
+++ src/hooks/use-logout.jsx
@@ -1,16 +1,19 @@
 import { signOut } from "firebase/auth";
 import auth from "../firebase/firebase-config";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "../store/auth-context";
 
 export default function useLogOut() {
   const [error, setError] = useState();
   const [pending, setPending] = useState();
+  const authCtx = useContext(AuthContext);
+  const { dispatch } = authCtx;
 
   const logOut = () => {
     setPending(true);
     signOut(auth)
       .then(() => {
-        console.log("Log out success");
+        dispatch({ type: "logout" });
       })
       .catch((error) => {
         setError(error.message);

cda946ca4dd21af45f9e89d054fb337904195b12
pytest <pytest514@gmail.com>
2023-05-24 00:38:16 +0800

    initial commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,24 @@
+import styled from "styled-components";
+
 export default function DashboardPage() {
-  return <h1>This is the project dashboard</h1>;
+  return (
+    <Wrapper>
+      <h1>This is the project dashboard</h1>;
+      <Form>
+        <label htmlFor="title" name="title">
+          Project title
+        </label>
+        <input type="text" id="title"></input>
+        <label htmlFor="title" name="title">
+          Details
+        </label>
+        <input type="text" id="title"></input>
+      </Form>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div``;
+const Form = styled.form`
+  display: grid;
+`;

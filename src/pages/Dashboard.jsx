afe598144147a14207c301e9a88eaf70815ef54a
pytest <pytest514@gmail.com>
2023-05-24 01:03:13 +0800

    feat: add project to firestore on submit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,24 +1,70 @@
+import { useState } from "react";
 import styled from "styled-components";
+import useFirestore from "../hooks/use-firestore";
+import { useRef } from "react";
 
 export default function DashboardPage() {
+  const [form, setForm] = useState({ title: "", details: "" });
+  const { addDocument } = useFirestore("projects");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    addDocument(form);
+  };
+
+  const titleChangeHandler = (e) => {
+    setForm({ ...form, title: e.target.value });
+  };
+
+  const detailsChangeHandler = (e) => {
+    setForm({ ...form, details: e.target.value });
+  };
+
   return (
     <Wrapper>
-      <h1>This is the project dashboard</h1>;
-      <Form>
+      <Header>Project dashboard</Header>
+      <Projects>
+        <h2>Project list</h2>
+        <ProjectList></ProjectList>
+      </Projects>
+      <Form onSubmit={handleFormSubmit}>
         <label htmlFor="title" name="title">
           Project title
         </label>
-        <input type="text" id="title"></input>
+        <input type="text" id="title" onChange={titleChangeHandler}></input>
         <label htmlFor="title" name="title">
           Details
         </label>
-        <input type="text" id="title"></input>
+        <textarea
+          rows="4"
+          cols="50"
+          id="details"
+          onChange={detailsChangeHandler}
+        ></textarea>
+        <button>Add</button>
       </Form>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div``;
+const Header = styled.h1`
+  grid-area: title;
+  margin-bottom: 32px;
+`;
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-areas:
+    "title title"
+    "projects form";
+  grid-template-columns: 1fr 300px;
+  gap: 16px;
+`;
 const Form = styled.form`
   display: grid;
+  grid-area: form;
+`;
+const ProjectList = styled.ul``;
+const Projects = styled.div`
+  grid-area: projects;
 `;

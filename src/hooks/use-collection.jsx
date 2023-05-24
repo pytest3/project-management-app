2269777e6af435262d1788a6c4a48501c515dc43
pytest <pytest514@gmail.com>
2023-05-24 14:40:05 +0800

    fix: code not firing as not placed in callback function

--- src/hooks/use-collection.jsx
+++ src/hooks/use-collection.jsx
@@ -1,20 +1,28 @@
-import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useCallback, useState } from "react";
 import { db } from "../firebase/firebase-config";
 
 onSnapshot;
 
 export default function useCollection(collectionName) {
-  const [collectionData, setCollectionData] = useState("");
+  const [collectionData, setCollectionData] = useState([]);
 
   const collectionRef = collection(db, collectionName);
 
   const getLiveCollection = useCallback(() => {
-    let data = [];
-    const unSub = onSnapshot(collectionRef, (snapshot) =>
-      snapshot.forEach((doc) => data.push({ id: doc.id, ...doc.data() }))
-    );
-    setCollectionData(data);
+    const unSub = onSnapshot(collectionRef, (snapshot) => {
+      let data = [];
+      snapshot.forEach((doc) => data.push({ id: doc.id, ...doc.data() }));
+      setCollectionData(data);
+      console.log(data);
+    });
+
     return unSub;
   }, []);
 

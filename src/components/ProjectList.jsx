b479e1dd3a1752c87f3134461fb5de5890aff880
pytest <pytest514@gmail.com>
2023-05-24 14:41:02 +0800

    initial commit

---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useMemo, memo } from "react";
+import useCollection from "../hooks/use-collection";
+
+const ProjectList2 = ({ data }) => {
+  const { collectionData, getLiveCollection } = useCollection("projects");
+
+  useEffect(() => {
+    const unSub = getLiveCollection();
+    return () => unSub();
+  }, []);
+
+  console.log("fired here?");
+
+  return collectionData.map((item) => (
+    <li key={item.id}>
+      <div>title: {item.title}</div>
+    </li>
+  ));
+};
+
+export default memo(ProjectList2);

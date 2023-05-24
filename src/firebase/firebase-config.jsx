4c8f71861a3d6ccdf98bae5be6d29645013a831b
pytest <pytest514@gmail.com>
2023-05-24 14:40:51 +0800

    chore: remove useless code

--- src/firebase/firebase-config.jsx
+++ src/firebase/firebase-config.jsx
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {

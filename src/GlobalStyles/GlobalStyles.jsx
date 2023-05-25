18a28faed496e0f9ac2a27439f1823258d4be492
pytest <pytest514@gmail.com>
2023-05-25 22:15:33 +0800

    style: added more global styles and corrected typo

--- src/GlobalStyles/GlobalStyles.jsx
+++ src/GlobalStyles/GlobalStyles.jsx
@@ -1,5 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
-import { COLORS, SPACING, FONT_SIZE } from '../constants';
+import {
+  COLORS,
+  SPACING,
+  FONT_SIZE,
+  FONT_WEIGHTS,
+  LINE_HEIGHT,
+  BORDER_RADIUS,
+} from '../constants';
 
 const GlobalStyles = createGlobalStyle`
     
@@ -115,71 +122,74 @@ html {
 
   --color-white: ${COLORS.white};
   --color-offblack: ${COLORS.offblack};
-  --color-gray-50: ${COLORS.gray[50]};
-  --color-gray-100: ${COLORS.gray[100]};
-  --color-gray-200: ${COLORS.gray[200]};
+
+  --color-gray-50: ${COLORS.gray['50']};
+  --color-gray-100: ${COLORS.gray['100']};
+  --color-gray-200: ${COLORS.gray['200']};
   
-  --color-gray-300: ${COLORS.gray[300]};
-  --color-gray-400: ${COLORS.gray[400]};
-  --color-gray-500: ${COLORS.gray[500]};
-  --color-gray-600: ${COLORS.gray[600]};
-  --color-gray-700: ${COLORS.gray[700]};
-  --color-gray-800: ${COLORS.gray[800]};
-  --color-gray-900: ${COLORS.blueGray[900]};
-  --color-blueGray-50: ${COLORS.blueGray[50]};
-  --color-bluegray-100: ${COLORS.blueGray[100]};
-  --color-blueGray-200: ${COLORS.blueGray[200]};
-  --color-blueGray-300: ${COLORS.blueGray[300]};
-  --color-blueGray-400: ${COLORS.blueGray[400]};
-  --color-blueGray-500: ${COLORS.blueGray[500]};
-  --color-blueGray-600: ${COLORS.blueGray[600]};
-  --color-blueGray-700: ${COLORS.blueGray[700]};
-  --color-blueGray-800: ${COLORS.blueGray[800]};
-  --color-blueGray-900: ${COLORS.blueGray[900]};
-  --color-blue-50: ${COLORS.blue[50]};
-  --color-blue-100: ${COLORS.blue[100]};
-  --color-blue-200: ${COLORS.blue[200]};
-  --color-blue-300: ${COLORS.blue[300]};
-  --color-blue-400: ${COLORS.blue[400]};
-  --color-blue-500: ${COLORS.blue[500]};
-  --color-blue-600: ${COLORS.blue[600]};
-  --color-blue-700: ${COLORS.blue[700]};
-  --color-blue-800: ${COLORS.blue[800]};
-  --color-blue-900: ${COLORS.blue[900]};
-  --color-blue-50: ${COLORS.blue[50]};
-  --color-green-100: ${COLORS.green[100]};
-  --color-green-200: ${COLORS.green[200]};
-  --color-green-300: ${COLORS.green[300]};
-  --color-green-400: ${COLORS.green[400]};
-  --color-green-500: ${COLORS.green[500]};
-  --color-green-600: ${COLORS.green[600]};
-  --color-green-700: ${COLORS.green[700]};
-  --color-green-800: ${COLORS.green[800]};
-  --color-green-900: ${COLORS.green[900]};
-  --color-red-300: ${COLORS.red[300]};
-  --color-red-500: ${COLORS.red[500]};
-  --color-red-700: ${COLORS.red[700]};
+  --color-gray-300: ${COLORS.gray['300']};
+  --color-gray-400: ${COLORS.gray['400']};
+  --color-gray-500: ${COLORS.gray['500']};
+  --color-gray-600: ${COLORS.gray['600']};
+  --color-gray-700: ${COLORS.gray['700']};
+  --color-gray-800: ${COLORS.gray['800']};
+  --color-gray-900: ${COLORS.gray['900']};
+
+  --color-blueGray-50: ${COLORS.blueGray['50']};
+  --color-blueGray-100: ${COLORS.blueGray['100']};
+  --color-blueGray-200: ${COLORS.blueGray['200']};
+  --color-blueGray-300: ${COLORS.blueGray['300']};
+  --color-blueGray-400: ${COLORS.blueGray['400']};
+  --color-blueGray-500: ${COLORS.blueGray['500']};
+  --color-blueGray-600: ${COLORS.blueGray['600']};
+  --color-blueGray-700: ${COLORS.blueGray['700']};
+  --color-blueGray-800: ${COLORS.blueGray['800']};
+  --color-blueGray-900: ${COLORS.blueGray['900']};
+
+  --color-blue-50: ${COLORS.blue['50']};
+  --color-blue-100: ${COLORS.blue['100']};
+  --color-blue-200: ${COLORS.blue['200']};
+  --color-blue-300: ${COLORS.blue['300']};
+  --color-blue-400: ${COLORS.blue['400']};
+  --color-blue-500: ${COLORS.blue['500']};
+  --color-blue-600: ${COLORS.blue['600']};
+  --color-blue-700: ${COLORS.blue['700']};
+  --color-blue-800: ${COLORS.blue['800']};
+  --color-blue-900: ${COLORS.blue['900']};
+  --color-blue-50: ${COLORS.blue['50']};
+  --color-green-100: ${COLORS.green['100']};
+  --color-green-200: ${COLORS.green['200']};
+  --color-green-300: ${COLORS.green['300']};
+  --color-green-400: ${COLORS.green['400']};
+  --color-green-500: ${COLORS.green['500']};
+  --color-green-600: ${COLORS.green['600']};
+  --color-green-700: ${COLORS.green['700']};
+  --color-green-800: ${COLORS.green['800']};
+  --color-green-900: ${COLORS.green['900']};
+  --color-red-300: ${COLORS.red['300']};
+  --color-red-500: ${COLORS.red['500']};
+  --color-red-700: ${COLORS.red['700']};
 
   /* Spacing 
   ===================================================================
   */
 
   --space-1: ${SPACING['1']};
-  --stralluepace-2: ${SPACING['2']};
+  --space-2: ${SPACING['2']};
   --space-3: ${SPACING['3']};
   --space-4: ${SPACING['4']};
   --space-5: ${SPACING['5']};
   --space-6: ${SPACING['6']};
   --space-7: ${SPACING['7']};
   --space-8: ${SPACING['8']};
-  --space-8: ${SPACING['9']};
-  --space-8: ${SPACING['10']};
-  --space-8: ${SPACING['11']};
-  --space-8: ${SPACING['12']};
-  --space-8: ${SPACING['13']};
-  --space-8: ${SPACING['14']};
-  --space-8: ${SPACING['15']};
-  --space-8: ${SPACING['16']};
+  --space-9: ${SPACING['9']};
+  --space-10: ${SPACING['10']};
+  --space-11: ${SPACING['11']};
+  --space-12: ${SPACING['12']};
+  --space-13: ${SPACING['13']};
+  --space-14: ${SPACING['14']};
+  --space-15: ${SPACING['15']};
+  --space-16: ${SPACING['16']};
 
 
   /* TYPOGRAPHY
@@ -197,17 +207,27 @@ html {
   --font-size-10: ${FONT_SIZE['10']};
   --font-size-11: ${FONT_SIZE['11']};
 
+  --font-weight-lighter:${FONT_WEIGHTS.lighter};
+  --font-weight-light:${FONT_WEIGHTS.light};
+  --font-weight-normal:${FONT_WEIGHTS.normal};
+  --font-weight-medium:${FONT_WEIGHTS.medium};
+  --font-weight-bold:${FONT_WEIGHTS.bold};
+  --font-weight-bolder:${FONT_WEIGHTS.bolder};
+
+  --line-height-small: ${LINE_HEIGHT.small};
+  --line-height-medium: ${LINE_HEIGHT.medium};
+  --line-height-large: ${LINE_HEIGHT.large};
 
 
+  /* BORDER
+===================================================================
+  */
 
-
-
-
+  --border-radius-small: ${BORDER_RADIUS.small};
+  --border-radius-medium: ${BORDER_RADIUS.medium};
+  --border-radius-large: ${BORDER_RADIUS.large};
+  --border-radius-larger: ${BORDER_RADIUS.larger};
 }
-
-
-
-
 `;
 
 export default GlobalStyles;
